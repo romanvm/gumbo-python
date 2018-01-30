c9a026a2f000564c5b5bc4fa9d6441b000c6d689
Roman Miroshnychenko <roman1972@gmail.com>
2018-01-30 18:27:36 +0200

    Extend C++ wrapper classes

--- src/bindings/gumbo_py.h
+++ src/bindings/gumbo_py.h
@@ -5,19 +5,21 @@
 #include <pybind11/pybind11.h>
 
 #include <string>
+#include <vector>
 #include <exception>
 #include <unordered_map>
+#include <array>
 
 namespace gumbo_python {
 
-	std::unordered_map<int,std::string> node_types{
-		{GUMBO_NODE_DOCUMENT, "document"},
-		{GUMBO_NODE_ELEMENT, "element"},
-		{GUMBO_NODE_TEXT, "text"},
-		{GUMBO_NODE_CDATA, "cdata"},
-		{GUMBO_NODE_COMMENT, "comment"},
-		{GUMBO_NODE_WHITESPACE, "whitespace"},
-		{GUMBO_NODE_TEMPLATE, "template"}
+	std::array<std::string, 7> node_types{
+		"document",
+		"element",
+		"text",
+		"cdata",
+		"comment",
+		"whitespace",
+		"template"
 	};
 
 	class TypeError : public std::exception {};
@@ -53,17 +55,63 @@ namespace gumbo_python {
 	class NodeWrapper {
 	private:
 		GumboNode* node_;
+		std::string tag_name_;
+		std::unordered_map<std::string, std::string> attributes_;
+		std::string text_;
+
+		void check_if_element(const std::string& err_message) {
+			if (node_->type != GUMBO_NODE_ELEMENT || node_->type != GUMBO_NODE_TEMPLATE)
+				throw TypeError(err_message);
+		}
 
 	public:
-		NodeWrapper(GumboNode* node) : node_(node) {}
+		NodeWrapper(GumboNode* node) : node_(node) {
+			if (node_->type == GUMBO_NODE_ELEMENT || node_->type == GUMBO_NODE_TEMPLATE) {
+				tag_name_ = gumbo_normalized_tagname(node_->v.element.tag);
+				GumboVector* raw_attributes = node_->v.element.attributes;
+				for (unsigned int i = 0; i < raw_attributes->length; ++i) {
+					GumboAttribute* attr = static_cast<GumboAttribute*>(raw_attributes[i]);
+					attributes_[attr->name] = attr->value;
+				}
+			}
+			else if (node_->type == GUMBO_NODE_TEXT || node_->type == GUMBO_NODE_CDATA ||
+					node_->type == GUMBO_NODE_COMMENT || node_->type == GUMBO_NODE_WHITESPACE) {
+				text_ = node_->v.text.text;
+			}
+		}
 
 		NodeVectorWrapper iter() {
-			if (node_->type != GUMBO_NODE_ELEMENT || node_->type != GUMBO_NODE_TEMPLATE)
-				throw TypeError("Node type '" + node_type() + "' is not iterable!");
+			check_if_element("Node type '" + node_type() + "' is not iterable!");
 			return NodeVectorWrapper(node_->v.element.children);
 		}
 
 		std::string node_type() { return node_types[node_->type]; }
+
+		std::unordered_map<std::string, std::string>& attributes() {
+			check_if_element("Node type '" + node_type() + "' has no attributes!");
+			return attributes_;
+		}
+
+		std::string tag_name() {
+			check_if_element("Node type '" + node_type() + "' is not a tag!");
+			return tag_name_;
+		}
+
+		std::string text() {
+			if (node_->type == GUMBO_NODE_TEXT || node_->type == GUMBO_NODE_CDATA ||
+					node_->type == GUMBO_NODE_COMMENT || node_->type == GUMBO_NODE_WHITESPACE)
+				return text_;
+			else
+				throw TypeError("Node '" + node_type() + "' is not a text node!");
+		}
+
+		unsigned int offset() {
+			if (node_->type == GUMBO_NODE_ELEMENT || node_->type == GUMBO_NODE_TEMPLATE)
+				return node_->v.element.start_pos.offset;
+			else
+				return node_->v.text.start_pos.offset;
+			throw TypeError("Node type '" + node_type() + "' does not have offset!");
+		}
 	};
 
 
